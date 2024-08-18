4db1e7c3be02ae53d0aa0a70c7721e0aae7c4d83
CAndresSierra <camilofullstack@gmail.com>
2024-08-17 19:54:13 -0500

    update: footer fixed

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -9,11 +9,12 @@ const Footer = () => {
           Contactame
           <span className="text-xl text-cyan-400 dark:text-accent">:</span>
         </h1>
-        <div className="flex gap-20">
-          <div className="flex gap-3 justify-center items-center">
-            <IoMdMail className="text-cyan-400 dark:text-accent text-xl" />
-            <div className="flex flex-col ">
-              <h1 className="text-cyan-400 dark:text-accent text-xl">
+
+        <div className="flex-col xl:flex xl:flex-row gap-20">
+          <div className="flex gap-3 xl:justify-center justify-start mb-10 xl:mb-0 items-center">
+            <IoMdMail className="text-cyan-400 dark:text-accent xl:text-xl" />
+            <div className="flex flex-col">
+              <h1 className="text-cyan-400 dark:text-accent xl:text-xl">
                 Correo Electrónico
               </h1>
               <p className="dark:text-white text-primary">
@@ -21,10 +22,11 @@ const Footer = () => {
               </p>
             </div>
           </div>
-          <div className="flex gap-3 justify-center items-center">
-            <BsFillTelephoneFill className="text-cyan-400 dark:text-accent text-xl" />
-            <div className="flex flex-col text-xl">
-              <h1 className="text-cyan-400 dark:text-accent text-xl">
+
+          <div className="flex gap-3 xl:justify-center justify-start  items-center">
+            <BsFillTelephoneFill className="text-cyan-400 dark:text-accent xl:text-xl" />
+            <div className="flex flex-col">
+              <h1 className="text-cyan-400 dark:text-accent xl:text-xl">
                 Teléfono
               </h1>
               <p className="dark:text-white text-primary">(+57) 304-331-5068</p>
